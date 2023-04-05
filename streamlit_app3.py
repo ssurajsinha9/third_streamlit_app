d6c103e8ddae9b5046fef4c029609368609b26ad
ssurajsinha9 <124761668+ssurajsinha9@users.noreply.github.com>
2023-04-05 17:04:00 +0530

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-
+from urllib.error import URLError
 
 
 streamlit.title('My Parents New Healthy Diner')
@@ -40,7 +40,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-from urllib.error import URLError
+
 
 
 
